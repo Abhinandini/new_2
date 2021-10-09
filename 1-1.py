f476e4b8a2c1008d0b90a31aca84583e1c60e07c
abhi <abhi@Abhinandinis-MacBook-Pro.local>
2021-10-08 19:41:47 -0600

    Adding new file

---
+++ 1-1.py
@@ -0,0 +1,21 @@
+from git import Repo
+#INITIALIZE A NEW REPOSITORY
+'''new_repo = Repo.init('new')
+print ("New repository was created")'''
+#OPEN AN EXISTING LOCAL REPO
+'''my_repo = Repo('new')
+print ("Opened")'''
+#CLONE A REMOTE REPOSITORY
+'''clone = Repo.clone_from('https://github.com/Abhinandini/new', '/Users/abhi/Desktop/GitRepository')'''
+# or clone via ssh (will use default keys)
+#git.Repo.clone_from('git@github.cim:DevDungeon/Cookbook', 'Cookbook-ssh')
+file_list = [
+    "/Users/abhi/Desktop/jennifer.jpeg",
+]
+repo = Repo('GitRepository')
+repo.index.add(file_list)
+#repo.index.add(update=True)
+repo.index.commit("Adding new file")
+origin = repo.remote(name='origin')
+origin.push()
+#git_push()
